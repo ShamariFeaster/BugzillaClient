093a5830d4d66cf94ce53ef84f9ccfc06a40c047
Mario Feaster <alistproducer2@gmail.com>
2011-11-04 15:41:20 -0400

    Added generalized JSON creation fuctions:
    made a few variables class members and have decided to make the httpclient
    static and a member of the class so I can just reuse it (with the cookies atta
    ched instead of saving the cookie, recreating a client, and reattaching them
    with every request.
    
    Conflicts:
    
    	src/edu/fsu/cs/bugzillaclient/BugzillaClientActivity.java
    
    Change-Id: I583c421ff805ee447cbcaffdb69ced1980aed0fb

--- src/edu/fsu/cs/bugzillaclient/BugzillaClientActivity.java
+++ src/edu/fsu/cs/bugzillaclient/BugzillaClientActivity.java
@@ -5,9 +5,13 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.cookie.Cookie;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -22,56 +26,113 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class BugzillaClientActivity extends Activity {
     /** Called when the activity is first created. */
     final String TAG = "Demo";
+    Boolean first_login = true;
+    static final DefaultHttpClient mHttpclient = new DefaultHttpClient();
+    final String mServerURL = "http://mobiserve.cs.fsu.edu/bugzilla/jsonrpc.cgi";
+
+    /*
+     * Constructs a proper formatted JSON array to hold parameters Bugzilla
+     * Webservice API functions using JSON-RPC create maps with the key/value
+     * parameter pairs and pass the to this function then pass the return value
+     * of this function to createJSONBugzillaRequest() forproperly formed JSON
+     * requests
+     */
+    private JSONArray createBugzillaParamsArray(Map... arguments) {
+        JSONObject json_params_object_child = new JSONObject();
+        JSONArray json_params_values_array = new JSONArray();
+
+        for (int var_arg_counter = 0; var_arg_counter < arguments.length; var_arg_counter++) {
+            Map this_map = arguments[var_arg_counter];
+            for (Iterator<Map> iter = this_map.entrySet().iterator(); iter.hasNext();) {
+                Map.Entry mapEntry = (Map.Entry) iter.next();
+                try {
+                    json_params_object_child.put((String) mapEntry.getKey(), mapEntry.getValue());
+                } catch (JSONException e1) {
+                    Log.v("createBugzillaParamsArray()",
+                            "Something is wrong with the JSON you passed in : first try block");
+                }
+            }
+        }
+        try {
+            json_params_values_array.put(0, json_params_object_child);
+        } catch (JSONException e) {
+            Log.v("createBugzillaParamsArray()",
+                    "Something is wrong with the JSON you passed in : second try block");
+        }
+        return json_params_values_array;
+    }
+
+    /*
+     * Constructs a proper formatted JSON object to send to Bugzilla Webservice
+     * API id - the "key" of the object should be the string "id" method - the
+     * "key" of the object should be the string "method" params - construct
+     * using createBugzillaParamsArray(Map... arguments)
+     */
+    private JSONObject createBugzillaJSONRequest(JSONObject id, JSONObject method, JSONArray params) {
+        JSONArray json_request_array = new JSONArray();
+        JSONObject json_request_object = new JSONObject();
+        try {
+            json_request_object.put("id", id.get("id"));
+            json_request_object.put("method", method.get("method"));
+            json_request_object.put("params", params);
+        } catch (JSONException e) {
+            Log.v("createBugzillaJSONRequest()", "Something is wrong with the JSON you passed in");
+        }
+        return json_request_object;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dummy);
-        final EditText methodFromInput = (EditText) findViewById(R.id.MethodEditText1);
         Button sendRequestButton = (Button) findViewById(R.id.SendRequestButton1);
 
         sendRequestButton.setOnClickListener(new OnClickListener() {
 
             public void onClick(View v) {
-                DefaultHttpClient httpclient = new DefaultHttpClient();
+
                 HttpEntity entity = null;
-                String login = null;
                 HttpResponse response = null;
+                String username = "alistproducer2@gmail.com";
+                String password = "_drumnba422";
 
-                String serverURL = "http://mobiserve.cs.fsu.edu/bugzilla/jsonrpc.cgi";
-                String method = "?method=Product.get_selectable_products";
-                String params = "&params=";
-                String thisMethod = methodFromInput.getText().toString();
-                login =
-                        "[{ \"Bugzilla_login\": \"alistproducer2@gmail.com\"," +
-                                "\"Bugzilla_password\": \"_drumnba422\"," +
-                                " \"Bugzilla_rememberLogin\":\"true\"}]";
-
+                JSONObject json_login = new JSONObject();
+                JSONArray paramArray = new JSONArray();
+                JSONObject rcp_call = new JSONObject();
                 try {
-                    login = URLEncoder.encode(login, "utf-8");
-                } catch (UnsupportedEncodingException e3) {
-                    // TODO Auto-generated catch block
-                    e3.printStackTrace();
-                }
-                serverURL += method + params + login;
+                    json_login.put("login", username);
+                    json_login.put("password", password);
+                    paramArray.put(0, json_login);
+                    rcp_call.put("params", paramArray);
+                    rcp_call.put("method", "User.login");
+                    rcp_call.put("id", 1);
+                    String login = rcp_call.toString();
+                    Log.d(TAG, login);
+                    try {
+                        StringEntity stringEntity = new StringEntity(login);
+                        HttpPost post = new HttpPost(mServerURL);
+                        post.setEntity(stringEntity);
+                        try {
+                            response = mHttpclient.execute(post);
+                        } catch (ClientProtocolException e2) {
+                            e2.printStackTrace();
+                        } catch (IOException e2) {
+                            e2.printStackTrace();
+                        }
 
-                Log.d(TAG, serverURL);
-                HttpGet httpget = new HttpGet(serverURL);
-
-                try {
-                    response = httpclient.execute(httpget);
-                } catch (ClientProtocolException e2) {
-                    // TODO Auto-generated catch block
-                    e2.printStackTrace();
-                } catch (IOException e2) {
-                    // TODO Auto-generated catch block
-                    e2.printStackTrace();
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                } catch (JSONException e4) {
+                    e4.printStackTrace();
                 }
 
                 if (response != null) {
@@ -91,7 +152,7 @@ public class BugzillaClientActivity extends Activity {
                     }
 
                     BufferedReader reader = new BufferedReader(
-                                     new InputStreamReader(content));
+                            new InputStreamReader(content));
                     String line;
                     try {
                         while ((line = reader.readLine()) != null) {
@@ -107,15 +168,16 @@ public class BugzillaClientActivity extends Activity {
                     try {
                         entity.consumeContent();
                     } catch (IOException e) {
-                        Log.d(TAG, "Error: UnsupportedEncodingException Caught - Line ~ 86");
+                        Log.d(TAG,
+                                "Error: UnsupportedEncodingException Caught - Line ~ 86");
                     }
                 }
 
-                System.out.println("Post logon cookies:");
-                List<Cookie> cookies = httpclient.getCookieStore().getCookies();
+                List<Cookie> cookies = mHttpclient.getCookieStore().getCookies();
                 if (cookies.isEmpty()) {
                     Log.d(TAG, "No cookies returned - Line ~ 94");
                 } else {
+                    first_login = false;
                     for (int i = 0; i < cookies.size(); i++) {
                         Log.d(TAG, cookies.get(i).toString());
                     }
@@ -129,4 +191,5 @@ public class BugzillaClientActivity extends Activity {
             }
         });
     }
+
 }
